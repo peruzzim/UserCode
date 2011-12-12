633016c96e1c4251750e3c9d7c80a1e99cd56691
peruzzi <>
2011-12-12 10:54:10 +0000

    *** empty log message ***

--- template_studies.C
+++ template_studies.C
@@ -49,34 +49,36 @@ void fit_dataset(const char* datafilename, const char* sigfilename, const char* 
     template_production helper;
 
     if (splitting=="EBEB") {
-      datafile->GetObject(helper.get_roovar_name(varname,0,0),roovar[0]);
-      datafile->GetObject(helper.get_roovar_name(varname,0,1),roovar[1]);
-      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EB"),TString("1")),roohist[0][0]);
-      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EB"),TString("2")),roohist[0][1]);
-      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EB"),TString("1")),roohist[1][0]);
-      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EB"),TString("2")),roohist[1][1]);
+      datafile->GetObject(helper.get_roovar_name(varname,0,0).Data(),roovar[0]);
+      datafile->GetObject(helper.get_roovar_name(varname,0,1).Data(),roovar[1]);
+      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EB"),TString("1")).Data(),roohist[0][0]);
+      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EB"),TString("2")).Data(),roohist[0][1]);
+      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EB"),TString("1")).Data(),roohist[1][0]);
+      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EB"),TString("2")).Data(),roohist[1][1]);
     }
-    else if (splitting="EBEE"){
+    else if (splitting=="EBEE"){
       datafile->GetObject(helper.get_roovar_name(varname,0,0),roovar[0]);
       datafile->GetObject(helper.get_roovar_name(varname,1,1),roovar[1]);
-      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EB"),TString("1")),roohist[0][0]);
-      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EE"),TString("2")),roohist[0][1]);
-      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EB"),TString("1")),roohist[1][0]);
-      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EE"),TString("2")),roohist[1][1]);
+      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EB"),TString("1")).Data(),roohist[0][0]);
+      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EE"),TString("2")).Data(),roohist[0][1]);
+      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EB"),TString("1")).Data(),roohist[1][0]);
+      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EE"),TString("2")).Data(),roohist[1][1]);
     }
-    else if (splitting="EEEE"){
+    else if (splitting=="EEEE"){
       datafile->GetObject(helper.get_roovar_name(varname,1,0),roovar[0]);
       datafile->GetObject(helper.get_roovar_name(varname,1,1),roovar[1]);
-      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EE"),TString("1")),roohist[0][0]);
-      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EE"),TString("2")),roohist[0][1]);
-      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EE"),TString("1")),roohist[1][0]);
-      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EE"),TString("2")),roohist[1][1]);
+      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EE"),TString("1")).Data(),roohist[0][0]);
+      sigfile->GetObject(helper.get_roohist_name(varname,TString("sig"),TString("EE"),TString("2")).Data(),roohist[0][1]);
+      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EE"),TString("1")).Data(),roohist[1][0]);
+      bkgfile->GetObject(helper.get_roohist_name(varname,TString("bkg"),TString("EE"),TString("2")).Data(),roohist[1][1]);
     }
 
-    datafile->GetObject(helper.get_roodset_name(varname,splitting),roodset);
+    datafile->GetObject(helper.get_roodset_name(varname,splitting).Data(),roodset);
+    cout << "using dset " << helper.get_roodset_name(varname,splitting).Data() << endl;
 
   }
  
+
   RooHistPdf sig1pdf("sig1pdf","sig1pdf",*(roovar[0]),*(roohist[0][0]));
   RooHistPdf sig2pdf("sig2pdf","sig2pdf",*(roovar[1]),*(roohist[0][1]));
   RooHistPdf bkg1pdf("bkg1pdf","bkg1pdf",*(roovar[0]),*(roohist[1][0]));
@@ -105,27 +107,34 @@ void fit_dataset(const char* datafilename, const char* sigfilename, const char* 
     roodset->plotOn(varframe[i]);
     model.plotOn(varframe[i],LineColor(kBlue));
     if (i==0){
-    sig1pdf.plotOn(varframe[i],Normalization(nsigsig.getVal()+nsigbkg.getVal(),RooAbsReal::NumEvent));
-    bkg1pdf.plotOn(varframe[i],Normalization(nbkgbkg.getVal()+nbkgsig.getVal(),RooAbsReal::NumEvent));
+      sig1pdf.plotOn(varframe[i],LineColor(kRed),Normalization(nsigsig.getVal()+nsigbkg.getVal(),RooAbsReal::NumEvent));
+      bkg1pdf.plotOn(varframe[i],LineColor(kGreen),Normalization(nbkgbkg.getVal()+nbkgsig.getVal(),RooAbsReal::NumEvent));
     }
     else if (i==1){
-    sig2pdf.plotOn(varframe[i],Normalization(nsigsig.getVal()+nbkgsig.getVal(),RooAbsReal::NumEvent));
-    bkg2pdf.plotOn(varframe[i],Normalization(nbkgbkg.getVal()+nsigbkg.getVal(),RooAbsReal::NumEvent));
+    sig2pdf.plotOn(varframe[i],LineColor(kRed),Normalization(nsigsig.getVal()+nbkgsig.getVal(),RooAbsReal::NumEvent));
+    bkg2pdf.plotOn(varframe[i],LineColor(kGreen),Normalization(nbkgbkg.getVal()+nsigbkg.getVal(),RooAbsReal::NumEvent));
     }
     
   }
 
 
-  TCanvas *c1 = new TCanvas();
-  c1->Divide(2,0);
-  c1->cd(1);
-  varframe[0]->Draw();
-  c1->cd(2);
-  varframe[1]->Draw();
-  c1->Update();
-  //  c1->SetLogy();
-  c1->SaveAs("fit.png");
-   
+  Float_t ntot=nsigsig.getVal()+nsigbkg.getVal()+nbkgsig.getVal()+nbkgbkg.getVal();
+  cout << "sigsig: " << nsigsig.getVal()/ntot << endl;
+  cout << "bkgsig: " << nbkgsig.getVal()/ntot << endl;
+  cout << "sigbkg: " << nsigbkg.getVal()/ntot << endl;
+  cout << "bkgbkg: " << nbkgbkg.getVal()/ntot << endl;
+
+
+  TCanvas *c1[2];
+  for (int i=0; i<2; i++){
+    c1[i] = new TCanvas();
+    c1[i]->cd();
+    varframe[i]->Draw();
+    c1[i]->SaveAs(Form("fit_%d.png",i));
+    c1[i]->Close();
+  }
+
+
 
 };
 
